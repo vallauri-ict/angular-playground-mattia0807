acc3d65646a83ea94d9e7ea6480bc6091c3b08c8
Mattia Tonello <m.tonello.0411@vallauri.edu>
2020-04-21 10:26:41 +0200

    my-recipes

--- my-recipes/src/app/app.component.spec.ts
+++ my-recipes/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-course'`, () => {
+  it(`should have as title 'my-recipes'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-course');
+    expect(app.title).toEqual('my-recipes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-course app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('my-recipes app is running!');
   });
 });
